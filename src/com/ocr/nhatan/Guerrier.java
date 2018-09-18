03808af5b03cdb525a8236c0363bb1ec138a8614
Nhatan <NMastner151217@gmail.com>
2018-09-19 00:50:20 +0100

    ajout de la methode attaque basique du Guerrier ajout de la methode attaque spéciale du Guerrier

--- src/com/ocr/nhatan/Guerrier.java
+++ src/com/ocr/nhatan/Guerrier.java
@@ -15,4 +15,30 @@ public class Guerrier extends Players{
     public String cri() {
         return "Woarg";
     }
+
+    /**
+     * cette méthode permet au Guerrier d'effectuer un dommage égal à la force du joueur sur l'adversaire
+     * @param p est un objet de type players
+     */
+
+    @Override
+    public void attaqueBasique(Players p) {
+        int damage = getForce();
+        System.out.println("Joueur " + getNumberOfPlayer() + " utilise Coup d'Epee et inflige " + damage + " dommages.");
+        p.coup(damage);
+    }
+
+    /**
+     * cette méthode est le coup de rage du Guerrier
+     * @param p
+     */
+    @Override
+    public void attaqueSpeciale(Players p) {
+        int damage = getForce() * 2;
+        int autoDamage = getForce() / 2;
+        System.out.println("Joueur " + getNumberOfPlayer() + " utilise Coup de rage et inflige " + damage + " dommages.");
+        p.coup(damage);
+        coup(autoDamage);
+    }
+
 }
--- test/com/ocr/nhatan/GuerrierTest.java
+++ test/com/ocr/nhatan/GuerrierTest.java
@@ -15,5 +15,20 @@ public class GuerrierTest {
             System.out.println(g1.cri());
             assertEquals("Woarg", g1.cri());
         }
-    
+
+    @Test
+    public void Given_Attack1Selected_When_PlayerTurn_Then_RemoveGoodAmountOfLifeToPersonnage2WithoutChangeLifeOfPersonnage1(){
+        g1.attaqueBasique(g2);
+        assertEquals(40,g2.getLife());
+        assertEquals(50,g1.getLife());
+    }
+
+    @Test
+    public void Given_Attack2Selected_When_PlayerTurn_Then_RemoveGoodAmountOfLifeToPersonnage2WithLooseLifeOfPersonnage1(){
+        g1.attaqueSpeciale(g2);
+        assertEquals(30,g2.getLife());
+        assertEquals(45,g1.getLife());
+    }
+
+
 }
