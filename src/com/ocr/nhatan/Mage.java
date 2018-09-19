19592bdc42650a7663457ca317c0e53815572ffc
Nhatan <NMastner151217@gmail.com>
2018-09-19 01:21:49 +0100

    ajout de la methode attaque basique du Mage, ajout de la methode attaque spéciale du Mage, ajout du cri spécifique au Mage.

--- src/com/ocr/nhatan/Mage.java
+++ src/com/ocr/nhatan/Mage.java
@@ -1,5 +1,48 @@
 package com.ocr.nhatan;
 
-public class Mage{
-  
+public class Mage extends Players {
+    private int defaultLife;
+
+    public Mage(int level, int force, int agility, int intelligence, int numberOfPlayer) {
+        super(level, force, agility, intelligence, numberOfPlayer);
+        this.defaultLife = level*5;
+    }
+
+    /**
+     * cri spécifique au Mage quand il début le jeux
+     * @return Abracadabra
+     */
+
+    @Override
+    public String cri() {
+        return "Abracadabra";
+    }
+
+    /**
+     * méthode permettant au mage d'infliger des domages égaux à son intelligence sur l'adversaire
+     * @param p objet de type Players
+     */
+
+    @Override
+    public void attaqueBasique(Players p) {
+        int damage = getIntelligence();
+        System.out.println("Joueur " + getNumberOfPlayer() + " utilise Boule de Feu et inflige " + damage + " dommages.");
+        p.coup(damage);
+    }
+
+    /**
+     * cette méthode permet au mage d'augmenter sa vie l'équivalent du double de son intelligence
+     * @param p
+     */
+
+    @Override
+    public void attaqueSpeciale(Players p) {
+        int health = getIntelligence() * 2;
+        if(getLife() + health >= defaultLife){
+            health = defaultLife - getLife();
+            soin(health);
+        }
+        else
+            soin(health);
+    }
 }
--- test/com/ocr/nhatan/MageTest.java
+++ test/com/ocr/nhatan/MageTest.java
@@ -1,5 +1,39 @@
 package com.ocr.nhatan;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MageTest {
-    
+
+    Mage m1 = new Mage(10, 0, 0, 10, 1);
+    Mage m2 = new Mage(10, 0, 0, 10, 2);
+
+    @Test
+    public void Given_GuerrierSelected_When_PlayersCreation_Then_DisplayTheScreamWord(){
+        System.out.println(m1.cri());
+        assertEquals("Abracadabra", m1.cri());
+    }
+
+    @Test
+    public void Given_AttaqueBasiqueSelected_When_PlayerTurn_Then_RemoveGoodAmountOfLifeToPlayers2WithoutChangeLifeOfPersonnage1(){
+        m1.attaqueBasique(m2);
+        assertEquals(40,m2.getLife());
+        assertEquals(50,m1.getLife());
+    }
+
+    @Test
+    public void Given_AttaueSpecialeSelectedWithFullLife_When_PlayerTurn_Then_DisplayMessageFor0LifeGiven(){
+        m1.attaqueSpeciale(m2);
+        assertEquals(50,m1.getLife());
+    }
+
+    @Test
+    public void Given_AttaqueSpecialeSelectedWithFullLife_When_PlayerTurn_Then_DisplayMessageFor20LifeGiven(){
+        m1.setLife(43);
+        m1.attaqueSpeciale(m2);
+        assertEquals(50,m1.getLife());
+    }
+
+
 }
