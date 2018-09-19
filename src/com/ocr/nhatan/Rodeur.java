7b3c9a72598f98d9ad34d0b0644ed9b68067d2b0
Nhatan <NMastner151217@gmail.com>
2018-09-19 01:04:02 +0100

    ajout de la methode attaque basique du Rodeur, ajout de la methode attaque spéciale du Rodeur, ajout du cri spécifique au Rodeur

--- src/com/ocr/nhatan/Rodeur.java
+++ src/com/ocr/nhatan/Rodeur.java
@@ -1,5 +1,40 @@
 package com.ocr.nhatan;
 
-public class Rodeur {
-  
+public class Rodeur extends Players {
+
+    public Rodeur(int level, int force, int agility, int intelligence, int numberOfPlayer) {
+        super(level, force, agility, intelligence, numberOfPlayer);
+    }
+
+    /**
+     * ici c'est un cri spécifique au  Rodeur quand il commence le jeux
+     * @return cruinnnggg
+     */
+
+    @Override
+    public String cri() {
+        return "cruinnnggg";
+    }
+
+    /**
+     * cette méthode permet au Rodeur de tirer à l'arc pour infliger des domages à son adversaire
+     * @param p
+     */
+
+    @Override
+    public void attaqueBasique(Players p) {
+        int damage = getAgility();
+        System.out.println("Joueur " + getNumberOfPlayer() + " utilise Tir à l'Arc et inflige " + damage + " dommages.");
+        p.coup(damage);
+    }
+
+    /**
+     * ici le Rodeur gagne la moitié de  son niveau en agilité
+     * @param p
+     */
+    @Override
+    public void attaqueSpeciale(Players p) {
+        int agilityWon = getLevel() / 2;
+        concentration(agilityWon);
+    }
 }
--- test/com/ocr/nhatan/RodeurTest.java
+++ test/com/ocr/nhatan/RodeurTest.java
@@ -1,5 +1,33 @@
 package com.ocr.nhatan;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RodeurTest {
-    
+
+    Rodeur r1 = new Rodeur(10, 0, 10, 0, 1);
+    Rodeur r2 = new Rodeur(10, 0, 10, 0, 2);
+
+    @Test
+    public void Given_GuerrierSelected_When_PersonnageCreation_Then_DisplayTheScreamWord(){
+        System.out.println(r1.cri());
+        assertEquals("Fsshhh", r1.cri());
+    }
+
+    @Test
+    public void Given_Attack1Selected_When_PlayerTurn_Then_RemoveGoodAmountOfLifeToPersonnage2WithoutChangeLifeOfPersonnage1(){
+        r1.attaqueBasique(r2);
+        assertEquals(40,r2.getLife());
+        assertEquals(50,r1.getLife());
+    }
+
+    @Test
+    public void Given_Attack2Selected_When_PlayerTurn_Then_CorrectAmountOfAddAgility(){
+        r1.attaqueSpeciale(r2);
+        assertEquals(15,r1.getAgility());
+
+    }
+
+
 }
